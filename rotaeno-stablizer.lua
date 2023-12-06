52763aa8fef2dcabe0edd6235fa2406b2aee36c3
Linnaea Von Lavia <linnaea@users.noreply.github.com>
2023-12-06 19:25:39 +0800

    可调采样位置，采样后隐去四角方块

--- rotaeno-stablizer.lua
+++ rotaeno-stablizer.lua
@@ -19,7 +19,6 @@ source_info.get_name = function()
   return "Rotaeno Stablizer"
 end
 
-
 source_info.create = function(settings, source)
   local data = {}
   data.source = source
@@ -36,9 +35,11 @@ source_info.create = function(settings, source)
   end
 
   data.params = {}
+  data.params.debug = obs.gs_effect_get_param_by_name(data.effect, "debug")
   data.params.aspect_s = obs.gs_effect_get_param_by_name(data.effect, "aspect_s")
   data.params.offset_s = obs.gs_effect_get_param_by_name(data.effect, "offset_s")
 
+  source_info.update(data, settings)
   return data
 end
 
@@ -69,12 +70,33 @@ source_info.video_render = function(data)
   local aspect_s = obs.vec2()
   obs.vec2_set(aspect_s, size / width, size / height)
   local offset_s = obs.vec2()
-  obs.vec2_set(offset_s, 4.5 / width, 4.5 / height)
+  obs.vec2_set(offset_s, data.sample_x / width, data.sample_y / height)
 
   obs.obs_source_process_filter_begin(data.source, obs.GS_RGBA, obs.OBS_NO_DIRECT_RENDERING)
 
   obs.gs_effect_set_vec2(data.params.aspect_s, aspect_s)
   obs.gs_effect_set_vec2(data.params.offset_s, offset_s)
+  obs.gs_effect_set_bool(data.params.debug, data.debug)
 
   obs.obs_source_process_filter_end(data.source, data.effect, size, size)
+end
+
+source_info.get_defaults = function(settings)
+  obs.obs_data_set_default_int(settings, "sample_x", 6)
+  obs.obs_data_set_default_int(settings, "sample_y", 6)
+  obs.obs_data_set_default_bool(settings, "debug", false)
+end
+
+source_info.get_properties = function(data)
+  local props = obs.obs_properties_create()
+  obs.obs_properties_add_int(props, "sample_x", "Sample X", 0, 32, 1)
+  obs.obs_properties_add_int(props, "sample_y", "Sample Y", 0, 32, 1)
+  obs.obs_properties_add_bool(props, "debug", "Debug")
+  return props
+end
+
+source_info.update = function(data, settings)
+  data.debug = obs.obs_data_get_bool(settings, "debug")
+  data.sample_x = obs.obs_data_get_int(settings, "sample_x") + 0.5
+  data.sample_y = obs.obs_data_get_int(settings, "sample_y") + 0.5
 end
