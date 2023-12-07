891a5de049103e1cfc87432c5f9261944fa2777e
Linnaea Von Lavia <linnaea@users.noreply.github.com>
2023-12-07 20:21:46 +0800

    增加输入朝向选项

--- rotaeno-stablizer.lua
+++ rotaeno-stablizer.lua
@@ -37,6 +37,7 @@ source_info.create = function(settings, source)
   data.params = {}
   data.params.debug = obs.gs_effect_get_param_by_name(data.effect, "debug")
   data.params.circle = obs.gs_effect_get_param_by_name(data.effect, "circle")
+  data.params.orient = obs.gs_effect_get_param_by_name(data.effect, "orient")
   data.params.aspect_s = obs.gs_effect_get_param_by_name(data.effect, "aspect_s")
   data.params.offset_s = obs.gs_effect_get_param_by_name(data.effect, "offset_s")
 
@@ -79,29 +80,38 @@ source_info.video_render = function(data)
   obs.gs_effect_set_vec2(data.params.offset_s, offset_s)
   obs.gs_effect_set_bool(data.params.debug, data.debug)
   obs.gs_effect_set_float(data.params.circle, data.circle)
+  obs.gs_effect_set_int(data.params.orient, data.orient)
 
   obs.obs_source_process_filter_end(data.source, data.effect, size, size)
 end
 
 source_info.get_defaults = function(settings)
-  obs.obs_data_set_default_int(settings, "sample_x", 8)
-  obs.obs_data_set_default_int(settings, "sample_y", 8)
+  obs.obs_data_set_default_int(settings, "sample_x", 4)
+  obs.obs_data_set_default_int(settings, "sample_y", 4)
   obs.obs_data_set_default_bool(settings, "debug", false)
   obs.obs_data_set_default_double(settings, "circle", 0.0)
+  obs.obs_data_set_default_int(settings, "orient", 0)
 end
 
 source_info.get_properties = function(data)
   local props = obs.obs_properties_create()
-  obs.obs_properties_add_float_slider(props, "circle", "Circular output", 0, 1, 0.001)
+  local orient = obs.obs_properties_add_list(props, "orient", "Orientation", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
+  obs.obs_property_list_add_int(orient, "90 CCW", -1)
+  obs.obs_property_list_add_int(orient, "Normal", 0)
+  obs.obs_property_list_add_int(orient, "90 CW", 1)
+  obs.obs_property_list_add_int(orient, "180", 2)
+
   obs.obs_properties_add_int(props, "sample_x", "Sample X", 0, 32, 1)
   obs.obs_properties_add_int(props, "sample_y", "Sample Y", 0, 32, 1)
+  obs.obs_properties_add_float_slider(props, "circle", "Circular output", 0, 1, 0.001)
   obs.obs_properties_add_bool(props, "debug", "Debug")
   return props
 end
 
 source_info.update = function(data, settings)
   data.debug = obs.obs_data_get_bool(settings, "debug")
-  data.circle = 0.5 - obs.obs_data_get_double(settings, "circle")/2
+  data.circle = 0.5 - obs.obs_data_get_double(settings, "circle") / 2
   data.sample_x = obs.obs_data_get_int(settings, "sample_x") + 0.5
   data.sample_y = obs.obs_data_get_int(settings, "sample_y") + 0.5
+  data.orient = obs.obs_data_get_int(settings, "orient")
 end
